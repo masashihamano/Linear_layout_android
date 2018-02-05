5490ba9a4a5cede1f09ab1c95c3109cceca1abeb
濱野将士 <masashihamano@mhp508.ad.nerc.ac.uk>
2018-02-06 00:14:32 +0530

    android_application_15

---
+++ app/src/main/java/edu/misao/linear_layout/Practice_Activity.java
@@ -0,0 +1,13 @@
+package edu.misao.linear_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Practice_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_practice_ );
+    }
+}
